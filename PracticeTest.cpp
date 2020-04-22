d509ea3ed1e028b23f9b6ed197118625990eabf9
Asheela Magwili <asheelamagwili@wlan-t-201-211-dhcp.CSUChico.EDU>
2020-04-22 12:43:16 -0700

    Adding sortDescending

--- PracticeTest.cpp
+++ PracticeTest.cpp
@@ -71,3 +71,41 @@ TEST(PracticeTest, simple)
     EXPECT_EQ(1, third);
 }
 
+TEST(PracticeTest, all_zeros)
+{
+    Practice obj;
+    int first = 0;
+    int second = 0;
+    int third = 0;
+    obj.sortDescending(first, second, third);
+    EXPECT_EQ(0, first);
+    EXPECT_EQ(0, second);
+    EXPECT_EQ(0, third);
+}
+
+TEST(PracticeTest, one_negative)
+{
+    Practice obj;
+    int first = 5;
+    int second = -3;
+    int third = 2;
+    obj.sortDescending(first, second, third);
+    EXPECT_EQ(5, first);
+    EXPECT_EQ(2, second);
+    EXPECT_EQ(-3, third);
+}
+
+TEST(PracticeTest, all_range)
+{
+    Practice obj;
+    int first = 0;
+    int second = -3;
+    int third = 5;
+    obj.sortDescending(first, second, third);
+    EXPECT_EQ(5, first);
+    EXPECT_EQ(0, second);
+    EXPECT_EQ(-3, third);
+}
+
+
+
