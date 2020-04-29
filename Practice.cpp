32a4ac6c452281338c1ffbcbdca7eaee50e41b93
Asheela Magwili <asheelamagwili@wlan-t-201-211-dhcp.CSUChico.EDU>
2020-04-28 22:20:32 -0700

    Fixing testing coverage

--- Practice.cpp
+++ Practice.cpp
@@ -19,12 +19,13 @@ void Practice::sortDescending(int & first, int & second, int & third)
     second = third;
     third = temp;
   }
+  /*
   if( first < third )
   {
     int temp = first;
     first = third;
     third = temp;
-  }
+  }*/
   if( first < second )
   {
     int temp = first;
